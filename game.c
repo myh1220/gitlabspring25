da5175aa516a78718b4354e0ee480e8e930ad651
gerard <gmccallion1@csudh.edu>
2025-04-16 01:38:09 +0000

    gmccallion Room 17

--- game.c
+++ game.c
@@ -17,7 +17,7 @@
 
 void room6game(void);
 void FarStarTrader(void);
-
+void room17(void);
 void room8game(void);
 
 
@@ -129,6 +129,7 @@ int main(int argc, char *argv[])
 			case 17:
 			{
 				puts("room17");
+				room17();
 				break;
 			}
 			case 18:
@@ -359,6 +360,10 @@ void FarStarTrader(void)
 void omarsUniqueFn(void)
 {
 	printf("Omar");
+}
+void room17(void)
+{
+	puts("cscuser17");
 }
 
 
